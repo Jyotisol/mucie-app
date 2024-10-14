64e9563326b2f5104554529692e48aa24d05e151
jyotisol <jyoti989solanki@gmail.com>
2024-10-14 16:17:05 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,7 @@ const App = () => {
 
   return (
     <div className="h-screen bg-custom-gradient">
-     {/* <RouterProvider router={router} /> */}
-
-    {/* // <div className="h-screen w-full bg-gradient-to-r from-custom-color to-custom-gradient via-red-500"> */}
+    
     <div className="h-[100%] flex ">
       {/* Sidebar takes 25% of the width */}
       <Sidebar className="w-1/4" />
@@ -26,25 +24,11 @@ const App = () => {
       {/* Player with custom styles */}
       <Player className="w-[30%]" />
     </div>
-    {/* <div className="">
-              <Outlet />
-            </div> */}
-
-    {/* Audio element to handle playback */}
+    
     <audio ref={audioRef} preload="auto" src={track.file}></audio>
   </div>
   )
 }
-//Routing
-// const router = createBrowserRouter([
-//   {
-//     path: "/login",
-//     element: <Login />,
-//   },
-//   {
-//     path: "/register",
-//     element: <Register />,
-//   },
-// ]);
+
 
 export default App
